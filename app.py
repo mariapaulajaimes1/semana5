558dca70d5e530e99434a53d9101215d3fca8e71
mariapaulajaimes1 <167145612+mariapaulajaimes1@users.noreply.github.com>
2024-08-15 10:18:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 from PIL import Image 
 
-st.title("TITULO PÁGINA")
+st.title("HOLA SOY HUGO")
 
-st.header( "Hola, cómo va todo")
-st.write ("Diferentes tipos de letra")
+st.header( "Soy un perro feliz")
+st.write ("Este soy yo")
+
 image= Image.open("Hugojaimes.jpeg")
-
 st.image(image,caption= "Hugojaimes")
+
+texto= st.text_input("Escribeme un mensaje", "este es mi mensaje")
+st.write( "El mensaje que mandaste es:", texto)
+
