3191ad2e0d3d16fea7c671eb9488f4b2cb3e1ec3
mariapaulajaimes1 <167145612+mariapaulajaimes1@users.noreply.github.com>
2024-08-15 10:26:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,10 +12,19 @@ st.image(image,caption= "Hugojaimes")
 texto= st.text_input("Escribeme un mensaje", "este es mi mensaje")
 st.write( "Gracias por tu mensaje:", texto)
 
-st.subheader("Ahora usemos 2 columnas")
+st.subheader("Ahora hablemos un poco")
 col1, col2 = st.columns(2)
 
 with col1:
   st.subheader("Esta es la segunda columna")
-  modo= st.radio(" que modalidad es la principal en tu interfaz", ("visual", "auditiva", "tactil"))
+  modo= st.radio("¿Qué te consideras?", ("perro", "humano", "otro"))
+
+if modo== "perro":
+  st.write(" Hola amigo canino")
+
+if modo== "humano":
+  st.write(" Hola amigo humano")
+
+if modo== "otro":
+  st.write( "Hola amigo confundido")
   
