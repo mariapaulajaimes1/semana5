018f7ce46ce6b3f70e17bcd3396c827d6d26f5f3
mariapaulajaimes1 <167145612+mariapaulajaimes1@users.noreply.github.com>
2024-08-15 10:33:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,15 +16,25 @@ st.subheader("Ahora hablemos un poco")
 col1, col2 = st.columns(2)
 
 with col1:
+
+st.subheader("Esta es la primera columna")
+  st.write("Las interfaces multimodales mejoran la experiencia de usuario")
+  resp = st.checkbox("Estoy de acuerdo")
+  if resp:
+    st.write("correcto!")
+
+with col2:
   st.subheader("Conozcámonos")
-  modo= st.radio("¿Qué te consideras?", ("perro", "humano", "otro"))
+  modo = st.radio("¿Qué te consideras?", ("perro", "humano", "otro"))
+  if modo == "perro":
+    st.write("Hola amigo canino")
+  if modo == "humano":
+    st.write(" Hola amigo humano")
+  if modo == "otro":
+     st.write("Hola amigo extraterrestre")
 
-if modo== "perro":
-  st.write(" Hola amigo canino")
 
-if modo== "humano":
-  st.write(" Hola amigo humano")
-
-if modo== "otro":
-  st.write( "Hola amigo confundido")
+st.subheader("usa los botones")
+if st.button(" presiona el botón")
+st.write(" Gracias por presionar")
   
