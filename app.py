55b03e75847b43bc589d982544c7a0e50ebd4b63
mariapaulajaimes1 <167145612+mariapaulajaimes1@users.noreply.github.com>
2024-08-15 10:22:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,3 +12,10 @@ st.image(image,caption= "Hugojaimes")
 texto= st.text_input("Escribeme un mensaje", "este es mi mensaje")
 st.write( "Gracias por tu mensaje:", texto)
 
+st.subheader("Ahora usemos 2 columnas")
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Esta es la segunda columna")
+  modo= st.radio(" que modalidad es la principal en tu interfaz", ("visual", "auditiva", "tactil"))
+  
