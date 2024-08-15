6d8cd8c5870d358a8b26f64bc029de1e47bcc36a
mariapaulajaimes1 <167145612+mariapaulajaimes1@users.noreply.github.com>
2024-08-15 10:07:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+from PIL import Image 
+
 st.title("TITULO PÁGINA")
+
 st.header( "Hola, cómo va todo")
 st.write ("Diferentes tipos de letra")
+image= Image.open(Hugojaimes.jpeg)
+
